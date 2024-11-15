b5e19e28dc12c8a3e6bac5489f479b1084de531a
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2024-11-15 22:12:59 +0530

    new update

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Middleware to serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home Route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Export the app for Vercel
+app.listen(3000)
