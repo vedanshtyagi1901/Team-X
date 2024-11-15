b6189fff7ce61a2952f6341cccfc413fdcae491b
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2024-11-15 17:40:26 +0530

    new update

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const path = require('path');
-const serverless = require('vercel-serverless-http');  // To convert Express app to serverless
 
 const app = express();
 
-// Middleware to serve static assets
-app.use(express.static('static'));
+// Middleware to serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Home Route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'templates', 'index.html'));
 });
 
-// Export the app to be used by Vercel
-module.exports = serverless(app);
+// Export the app for Vercel
+module.exports = app;
